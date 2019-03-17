3e3272ccda65269c0de7d1bca44caeaddad94e95
Admin <admin@Admins-Mac-mini.local>
2019-03-17 16:37:05 -0700

    adds fields to ui

--- public/main.js
+++ public/main.js
@@ -14,8 +14,13 @@ function getStreak() {
 $(document).ready(function(){
     $("button").click(function(e){
         e.preventDefault();
-        var input = $('#key').val();
-        console.log(`button was clicked ${input}`);
+        var students = [];        
+        var wakatimekey = $('#wakatimekey').val();
+        var firstname = $('#firstname').val();
+        var lastname = $('#lastname').val();
+        students.push({firstname, lastname, wakatimekey});
+        console.log(`button was clicked ${JSON.stringify(students)}`);
+        $('ul').append(`<li><a href="#">${students[0].firstname} ${students[0].lastname}</a> <a href="#">delete</a></li>`)
         getStreak();
     });
 });
