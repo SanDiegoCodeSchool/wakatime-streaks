188c7db71ea88061612ff89d6972cb83b6df5f55
Mike Roberts <mike@sdcs.io>
2019-04-28 17:52:38 -0700

    wip

--- public/privacy.js
+++ public/privacy.js
@@ -1,43 +1,4 @@
-function getAllStudents(){
-    $.ajax({
-        url: "/api/Students",
-        success: function(result) {
-            let students = result.map(function(student){
-                return `<li id="${student.id}">${student.firstname} ${student.lastname} <a href="#">delete</a></li>`  
-            });
-            $('ul').append(students.join(""));
-            $("li").click(function() {
-                getStreak($(this).attr('id'));
-            });
-        },
-        failure: function(response) {
-            console.log(response);
-        }
-    });
-}
-function getStreak(id) {
-    $.ajax({
-        url: `/api/Students/getStreaks?id=${id}`,
-        success: function(result) {
-            $("#wakaResult").html(`Streak: ${result.days}`);
-        },
-        failure: function(response) {
-            console.log(response);
-        }
-    });
-}
-function postStudent(student){
-    $.ajax({
-        type: "post",
-        url: "/api/Students",
-        data: student,
-        success: function(result) {
-        },
-        failure: function(response) {
-            console.log(response);
-        }
-    });
-}
+
 $(document).ready(function(){
     getAllStudents();
     $("button").click(function(e){
